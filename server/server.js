d5687e6d64e7940d9cca7dcaf2e8782b7914fddd
Antonio Sanchez <antonio.san.87@gmail.com>
2019-11-01 16:27:09 +0100

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,36 +13,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('Get Usuario')
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-})
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('Delete Usuario')
-})
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando el puerto: ', 3000);
+    console.log('Escuchando el puerto: ', process.env.PORT);
 });
